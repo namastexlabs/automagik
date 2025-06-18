21f40955335b380ee11a02cc5c00426cac03aeb9
Felipe Rosa <felipe@namastex.ai>
2025-06-17 21:57:08 -0300

    fix: workflows

---
+++ ui/.next/static/chunks/app_page_tsx_3ee7da22._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_e780a550._.js",
+  "static/chunks/node_modules__pnpm_4ed8d83f._.js"
+],
+    source: "dynamic"
+});
